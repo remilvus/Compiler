1eba7c632cda9622f6d04cdef2285ff0e1956125
sheldak <samuel.heldak@gmail.com>
2020-10-23 17:15:30 +0200

    Add basic logic, matrices and if-else statements to parser

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import parser
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        # filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example_2.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
--- parser.py
+++ parser.py
@@ -1,35 +1,63 @@
 import ply.yacc as yacc
-from scanner import tokens
+from scanner import *
 
 
-
-def p_term(p):
-    'term : INT'
-    p[0] = p[1]
-
-
-def p_binary_operators(p):
-    '''term : term PLUS term
-                  | term MINUS term
-                  | term '' term
-                  | term '/' term'''
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    elif p[2] == '':
-        p[0] = p[1] * p[3]
-    elif p[2] == '/':
-        p[0] = p[1] / p[3]
+precedence = (
+    ('nonassoc', 'JUST_IF'),
+    ('left', 'EQ'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+)
 
 
-parser = yacc.yacc()
+def p_number(p):
+    '''number : INT
+              | FLOAT'''
 
-while True:
-    try:
-        s = input()
-    except EOFError:
-        break
-    if not s: continue
-    result = parser.parse(s)
-    print(result)
+
+def p_expression(p):
+    '''expression : ID
+                  | number
+                  | STRING'''
+
+
+def p_vector(p):
+    '''inner_vector : expression ',' expression
+                    | inner_vector ',' expression
+        vector      : '[' expression ']'
+                    | '[' inner_vector ']' '''
+
+
+def p_matrix(p):
+    '''inner_matrix : vector
+                    | inner_matrix ',' vector
+       matrix       : '[' inner_matrix ']' '''
+
+
+def p_plus(p):
+    '''expression : expression PLUS expression'''
+
+
+def p_equal(p):
+    '''expression : expression EQ expression'''
+
+
+def p_statement(p):
+    '''statement : expression ';'
+                 | '{' statement '}' '''
+
+
+def p_if_statement(p):
+    '''statement : IF '(' expression ')' statement %prec JUST_IF
+                 | IF '(' expression ')' statement ELSE statement'''
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')"
+              .format(p.lineno, find_column(p.lexer.lexdata, p.lexer.lexpos), p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+parser = yacc.yacc(start='statement')
