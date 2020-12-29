db9d99037d63ed430184384f645c48070db570d8
Adam Kania <remilvus@gmail.com>
2020-12-29 12:28:15 +0100

    [Interpreter] Change way of returning values; Add CompareExpr, UnaryMinus & Range

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -17,7 +17,16 @@ class Interpreter(object):
                           ".+": operator.add,
                           ".-": operator.sub,
                           "./": operator.truediv,
-                          ".*": operator.mul}
+                          ".*": operator.mul
+                          }
+
+        self.comparison_operators = {"==": operator.eq,
+                                     "!=": operator.ne,
+                                     "<": operator.lt,
+                                     "<=": operator.le,
+                                     ">": operator.gt,
+                                     ">=": operator.ge
+                                     }
 
     def generic_visit(self, node: Node):
         for child in node.children:
@@ -41,97 +50,107 @@ class Interpreter(object):
     def visit(self, node: Empty):
         pass
 
-    @when(StatementsList)
-    def visit(self, node: StatementsList):
-        for statement in node.statements_list:
-            self.visit(statement)
-
     @when(Number)
     def visit(self, node: Number):
-        node.value = node.number
-        node.type = Type.get_type(node.number)
+        return node.number
 
     @when(Expression)
     def visit(self, node: Expression):
         if Type.get_type(node.expression) == Type.STRING:
-            node.value = node.expression
+            return node.expression
         else:
-            self.visit(node.expression)
-            node.value = node.expression.value
+            return self.visit(node.expression)
 
     @when(InnerVector)
     def visit(self, node: InnerVector):
-        node.value = []
-        for expression in node.inner_vector:
-            self.visit(expression)
-            node.value.append(expression.value)
+        return [self.visit(expression) for expression in node.inner_vector]
 
     @when(Vector)
     def visit(self, node: InnerVector):
-        self.visit(node.inner_vector)
-        inner = node.inner_vector
+        inner = self.visit(node.inner_vector)
 
-        if reduce(lambda x,y: x and isinstance(y, np.ndarray), inner.value, True):
+        if reduce(lambda x,y: x and isinstance(y, np.ndarray), inner, True):
             # matrix
-            node.value = np.stack(inner.value)
+            return np.stack(inner)
         elif reduce(lambda x,y: x and (isinstance(y, int) or isinstance(y, float)),
-                    inner.value,
+                    inner,
                     True):
-            node.value = np.array(inner.value)
+            return np.array(inner)
         else:
             # vector
-            node.value = inner.value
+            return inner.value
 
     @when(Matrix)
     def visit(self, node: Matrix):
-        self.generic_visit(node)
-
-        size = node.argument.value
+        size = self.visit(node.argument)
         type = node.matrix_type.lower()
+
         if type == "eye":
             shape = size
         else:
             shape = (size, size)
-        node.value = {"ones": np.ones,
-                      "zeros": np.zeros,
-                      "eye": np.eye}[type](shape, dtype=np.float)
+        return {"ones": np.ones,
+                "zeros": np.zeros,
+                "eye": np.eye}[type](shape, dtype=np.float)
 
-    @when(AssignExpr)
-    def visit(self, node: AssignExpr):
-        self.generic_visit(node)
+    @when(Range)
+    def visit(self, node: Range):
+        from_index = self.visit(node.from_index)
+        to_index = self.visit(node.to_index)
+        return range(from_index, to_index)
 
-        if type(node.left.slice_or_id) is str:  # id
-            self.memory_stack.set(node.left.slice_or_id, node.right.value)
-        else:
-            assert type(node.left) is Slice
-            # todo
-            raise NotImplemented
+    @when(UnaryMinus)
+    def visit(self, node: UnaryMinus):
+        return -self.visit(node.value)
 
     @when(BinExpr)
     def visit(self, node: BinExpr):
-        self.generic_visit(node)
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        operator = node.operator
 
-        left = node.left.value
-        right = node.right.value
-        if node.operator == "*" and isinstance(left, np.ndarray) and isinstance(right, np.ndarray):
-            node.value = left @ right
+        if operator == "*" and isinstance(left, np.ndarray) and isinstance(right, np.ndarray):
+            return left @ right
         else:
-            node.value = self.operators[node.operator](left, right)
+            return self.operators[operator](left, right)
 
     @when(MatrixBinExpr)
     def visit(self, node: MatrixBinExpr):
-        self.generic_visit(node)
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        operator = node.operator
 
-        left = node.left.value
-        right = node.right.value
-        node.value = self.operators[node.operator](left, right)
+        return self.operators[operator](left, right)
+
+    @when(Transposition)
+    def visit(self, node: Transposition):
+        matrix = self.visit(node.matrix)
+        return np.transpose(matrix)
+
+    @when(CompareExpr)
+    def visit(self, node: CompareExpr):
+        left = self.visit(node.left)
+        right = self.visit(node.right)
+        operator = node.operator
+
+        return self.comparison_operators[operator](left, right)
+
+    @when(SliceArgument)
+    def visit(self, node: SliceArgument):
+        pass
+        # todo
+
+    @when(Slice)
+    def visit(self, node: Slice):
+        pass
+        # todo
 
     @when(SliceOrID)
     def visit(self, node: SliceOrID):
         if type(node.slice_or_id) is str:  # id
             node.id = node.slice_or_id
             try:
-                node.value = self.memory_stack.get(node.slice_or_id)
+                return self.memory_stack.get(node.slice_or_id)
             except KeyError:
                 pass
         else:
@@ -139,25 +158,62 @@ class Interpreter(object):
             pass #todo
             # visit
 
+    @when(AssignExpr)
+    def visit(self, node: AssignExpr):
+        left_value = self.visit(node.left) # may be none
+        left = node.left.id
+        right = self.visit(node.right)
+        op = node.operator
+
+        if len(op) == 2:
+            bin_op = op[0]
+            assert type(left_value) is not None
+            right = self.operators[bin_op](left_value, right)
+
+        if type(left) is str:  # id
+            self.memory_stack.set(left, right)
+        else:
+            assert type(node.left) is Slice
+            # todo
+            raise NotImplemented
+
+
+    @when(StatementsList)
+    def visit(self, node: StatementsList):
+        for statement in node.statements_list:
+            self.visit(statement)
+
+    @when(Return)
+    def visit(self, node: Return):
+        pass
+        # todo
+
+    @when(CodeBlock)
+    def visit(self, node: CodeBlock):
+        pass
+        # todo
+
+    @when(LoopStatement)
+    def visit(self, node: LoopStatement):
+        pass
+        # todo
+
+    @when(For)
+    def visit(self, node: For):
+        pass
+        # todo
+
+    @when(While)
+    def visit(self, node: While):
+        pass
+        # todo
+
+    @when(If)
+    def visit(self, node: If):
+        pass
+        # todo
+
     @when(Print)
     def visit(self, node: Print):
-        self.generic_visit(node)
-        print(", ".join(map(str, node.value.value)))
-
-    # todo:
-    # # simplistic while loop interpretation
-    # @when(ast.WhileInstr)
-    # def visit(self, node):
-    #     r = None
-    #     while node.cond.accept(self):
-    #         r = node.body.accept(self)
-    #     return r
-
-    # def visit_range(self, node: Range):
-    # def visit_unary_minus(self, node: UnaryMinus):
-    # def visit_transposition(self, node: Transposition):
-    # def visit_compare_expr(self, node: CompareExpr):
-    # def visit_slice_argument(self, node: SliceArgument):
-    # def visit_slice(self, node: Slice):
-    # def visit_loop_statement(self, node: LoopStatement):    def visit_for(self, node: For):    def visit_while(self, node: While):
-    # def visit_if(self, node: If):
+        values = self.visit(node.value)
+        print(", ".join(map(str, values)))
